a5c3e658100c7c32f08321d05f38b52103a71f30
Susanne03 <151035@csgaugustinus.nl>
2019-11-20 09:50:21 +0000

    Opdracht 3 af

--- JS/H1/H1O03.js
+++ JS/H1/H1O03.js
@@ -3,16 +3,22 @@ function setup() {
   background('white');
   canvas.parent('processing');
   noLoop();
-  // colorMode(RGB,255,255,255,1);  
+ colorMode(RGB,255,255,255,1);  
 }
 
 function draw() {
   noStroke();
-  // fill(0,0,255,0.3);
-  fill('red');
+    fill(0,0,255,0.3);
+  //fill('red');
   rect(0,150,450,150);
-  fill(0,255,0);
+  //fill(0,255,0);
   rect(0,0,150,450);
-  fill('blue');  
+  // fill(0,0,255);  
   rect(75,0,375,450);
+
+    noStroke();
+    ellipse(450,225,450);
+
+    noStroke();
+    triangle(225,0,450,225,225,450);
 }
