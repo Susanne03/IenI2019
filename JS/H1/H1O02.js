910b9d7bb32a9a6e0f628b81396f6794833132cd
Susanne03 <151035@csgaugustinus.nl>
2019-11-20 09:01:00 +0000

    Opdracht 2 af

--- JS/H1/H1O02.js
+++ JS/H1/H1O02.js
@@ -9,4 +9,25 @@ function draw() {
   noStroke();
   fill('darkred');
   triangle(225,0,0,450,450,450);
+
+
+  noStroke();
+  fill('pink');
+  triangle(0,225,450,0,450,450);
+
+  noStroke();
+  fill('purple');
+  triangle(450,225,0,0,0,450);
+}
+
+function draw(){
+strokeWeight(5);
+stroke('pink');
+fill('darkred'); 
+beginShape(); 
+vertex(225,115);
+vertex(300,225);
+vertex(225,335);
+vertex(0,225);
+endShape(CLOSE);
 }
