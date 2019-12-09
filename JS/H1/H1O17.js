1f2ca6246813ad8281d7850306dfa57c9db872ef
Susanne03 <151035@csgaugustinus.nl>
2019-12-09 08:12:29 +0000

    opdr 17 af

--- JS/H1/H1O17.js
+++ JS/H1/H1O17.js
@@ -10,9 +10,16 @@ function draw() {
   background('cornflowerblue');
   schaal = 1 + mouseY / height;
 
+  tekenZon(mouseX,schaal);
+
   // teken de grond
   fill('wheat');
   rect(0,250,width,height - 250);  
+
+
+  tekenBoom(50);
+  tekenBoom(100);
+  tekenBoom(150);
 
   tekenHuis();
 
@@ -41,4 +48,12 @@ function tekenBoom(x) {
   fill('olive');
   ellipse(x + 20,130,100,150);
   pop();
+}
+
+function tekenZon(x,s) {
+ push()
+ fill('red');
+ scale(s);
+ ellipse(x,200,300,300);
+ pop();
 }
